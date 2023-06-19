61ef09b1be6e9da7f49eb6d3d926f277b8911a7f
Palesangobeni06 <palesangobeni06@gmail.com>
2023-06-19 10:09:37 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char c;
+	
+	int e;
+
+	c = 'a';
+	e = 0;
+	while
+		(e < 10) {
+			putchar(e + '0');
+			e++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
