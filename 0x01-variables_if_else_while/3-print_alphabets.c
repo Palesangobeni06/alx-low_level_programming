ae8d23ca2f51dd2c773a0453f932595a4d3ad608
Palesangobeni06 <palesangobeni06@gmail.com>
2023-06-21 10:10:30 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	char e;
+
+	c = 'a';
+	e = 'A';
+	while 
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(e <= 'z') {
+			putchar(e);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
