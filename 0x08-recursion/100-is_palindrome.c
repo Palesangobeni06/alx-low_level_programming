f185c95a82c7cf669637eae3d9db9c4716fa1051
Palesangobeni06 <palesangobeni06@gmail.com>
2023-07-05 09:42:14 -0700

    0x08-recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursions(char *s);
+
+/**
+ * is _palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is 
